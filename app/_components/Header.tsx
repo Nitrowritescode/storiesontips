5055f7610e095e8a118a19669d5dee8c4384ef63
Nitrowritescode <swarsehgal123@gmail.com>
2025-01-23 16:47:34 +0530

    updated header.tsx component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -20,7 +20,7 @@ export function Header() {
   }
 
   return (
-    <nav className="bg-[#b28ed3] dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-[#b28ed3] fixed w-full z-20 top-0 start-0">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2 px-4">
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <div className="rounded-full bg-purple-600 p-2">
