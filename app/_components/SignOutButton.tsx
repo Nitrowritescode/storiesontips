52f85fdee3ab3ecb9974847b522d10732cd2d5e8
Nitrowritescode <swarsehgal123@gmail.com>
2025-01-30 22:35:57 +0530

    fixed signout flow

--- app/_components/SignOutButton.tsx
+++ app/_components/SignOutButton.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { useClerk } from "@clerk/nextjs";
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
 
 
 export const SignOutButton = () => {
   const { signOut } = useClerk();
   
   return (
-    <button
-      onClick={() => signOut({ redirectUrl: '/' })}
+   
+    <Button as={Link} href="/"
+      onPress={() => signOut({ redirectUrl: '/' })}
       className="bg-blue-600 text-white px-3 py-2 w-full md:rounded-xl"
     >
       Sign Out
-    </button>
+    </Button>
   );
 };
