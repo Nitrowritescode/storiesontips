42d1893530052f50dcc0d553ac1605e4d0619f68
Nitrowritescode <swarsehgal123@gmail.com>
2025-01-30 22:29:45 +0530

    fixing signout flow

--- app/_components/SignOutButton.tsx
+++ app/_components/SignOutButton.tsx
@@ -1,25 +1,14 @@
 "use client";
 
 import { useClerk } from "@clerk/nextjs";
-import { error } from "console";
-import { useRouter } from "next/navigation";
+
 
 export const SignOutButton = () => {
   const { signOut } = useClerk();
-  const router = useRouter();
-
-  const handleSignOut = async () => {
-    try{await signOut();
-    }catch(error) {
-      console.log(error)
-    }
-      // redirect to homepage
-    router.push("/");
-  };
-
+  
   return (
     <button
-      onClick={handleSignOut}
+      onClick={() => signOut({ redirectUrl: '/' })}
       className="bg-blue-600 text-white px-3 py-2 w-full md:rounded-xl"
     >
       Sign Out
