1322b94bae9a32731620bccd81dc8b18fc2d28ef
Nitrowritescode <swarsehgal123@gmail.com>
2025-03-01 01:24:20 +0530

    updated landing  page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import { LandingFeatures } from "./_components/LandingFeatures";
 import LandingHero from "./_components/LandingHero";
 
 export default function Home() {
-  return <div className="bg-[url('/landing/background3.webp')] bg-cover bg-center backdrop-blur-3xl" >
+  return <div  className="bg-[url('/landing/background3.webp')] bg-cover bg-center backdrop-blur-3xl" >
   <LandingHero/>
   <LandingFeatures/>
 
