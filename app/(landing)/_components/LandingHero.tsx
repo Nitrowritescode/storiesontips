871343d8c3ea6bc4690d2a386a341800becca305
Nitrowritescode <swarsehgal123@gmail.com>
2025-03-01 01:34:25 +0530

    fixed responsiveness issue on landing page

--- app/(landing)/_components/LandingHero.tsx
+++ app/(landing)/_components/LandingHero.tsx
@@ -5,7 +5,7 @@ export default function LandingHero() {
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="text- 2xl md:text-4xl lg:text-6xl mx-auto p-8 rounded-xl font-normal text-white dark:text-neutral-400 bg-darkestneonblue/80">
+      <div className="text-2xl md:text-4xl lg:text-6xl mx-auto p-8 rounded-xl font-normal text-white dark:text-neutral-400 bg-darkestneonblue/80">
         Make
         <FlipWords words={words} /> <br />
         stories with <span className="text-neonblue font-bold">StoriesOnTips</span>
