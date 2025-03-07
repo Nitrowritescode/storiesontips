7bdadf8a997d85bd158debb88f45e255ef48c3be
Nitrowritescode <swarsehgal123@gmail.com>
2025-03-07 10:06:59 +0530

    updated login pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,31 @@
-'use client';
+"use client";
 
-import { SignIn } from '@clerk/nextjs';
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center mx-auto px-4 bg-[#000015]">
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 place-items-center mx-auto bg-[#000015]">
       <div className="max-w-md w-full mx-auto">
-        <SignIn/>
+        <div className="flex flex-col justify-center items-left w-full text-center pb-4">
+          <h1 className="text-2xl  text-white font-bold">Welcome Back</h1>
+          <p className="text-l text-white/50">
+          Get back to creating magical tales for fun and education
+          </p>
+        </div>
+        <div className="pl-6">
+          <SignIn />
+        </div>
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          width={400}
+          height={400}
+          alt="login page image"
+          className="bg-cover h-full w-full"
+          src="/landing/background2.webp"
+        />
       </div>
     </div>
-
   );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,14 +1,31 @@
-'use client'
+"use client";
 
-import { SignUp } from "@clerk/nextjs"
+import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center mx-auto bg-[#000015]">
-      <div className="max-w-md bg-white mx-auto">
-        <SignUp/>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 place-items-center justify-center mx-auto bg-[#000015]">
+      <div className="max-w-md w-full mx-auto">
+        <div className="flex flex-col justify-center items-left w-full text-center pb-2">
+          <h1 className="text-2xl  text-white font-bold">Welcome To The Journey</h1>
+          <p className="text-l text-white/50">
+            Start creating magical tales for fun and education
+          </p>
+        </div>
+        <div className="pl-6">
+          <SignUp />
+        </div>
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          width={400}
+          height={400}
+          alt="login page image"
+          className="bg-cover h-full w-full"
+          src="/landing/background2.webp"
+        />
       </div>
     </div>
-  )
+  );
 }
-
